8324a1f6104190cd681d8da9e4c7f9f82d5d710d
Alexander Riccio <test35965@gmail.com>
2019-05-16 00:21:39 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -21,6 +21,7 @@ def times_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   number_of_times.times do
     
+  end
 end
 
 def while_iterator(number_of_times)
